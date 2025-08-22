06a76619938daecb2e55102e7f4b5a6a024393ab
Hernandes de Castro Guereta <132833897+Nandesfront@users.noreply.github.com>
2025-08-22 01:39:56 +0000

    aula2

---
+++ src/main/java/app/Aluno.java
@@ -0,0 +1,9 @@
+package app;
+
+@Getter
+@Setter
+public class Aluno {
+    private String nome;
+    private int idade;
+
+
---
+++ src/main/java/app/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+package app;
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        Aluno al1 = new Aluno();
+        al1.setNome("Marco");
+        al1.setIdade(20);
+        System.out.println("Ola mundo");
+    }
+}
